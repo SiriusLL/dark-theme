bc2ac465cd4a1ee93a6aafa577a3bb3b064204b2
SiriusLL <sirius.love.light@gmail.com>
2022-02-08 00:39:18 -0700

    logic for useLocalStorate and switchTheme

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import useLocalStorage from "use-local-storage";
 import "./index.css";
 
 function App() {
+  const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "light");
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
   return (
-    <div className="app">
+    <div className="app" data-theme={theme}>
       <div className="login">
         <h1>Login</h1>
         <div className="container">
@@ -36,8 +42,11 @@ function App() {
         </div>
         <div className="theme-toggle">
           <h2>Light Theme</h2>
-          <i class="fas fa-toggle-on" />
-          <i class="fas fa-toggle-off" />
+          {theme === "dark" ? (
+            <i class="fas fa-toggle-on" onClick={switchTheme} />
+          ) : (
+            <i class="fas fa-toggle-off" onClick={switchTheme} />
+          )}
         </div>
       </div>
     </div>
